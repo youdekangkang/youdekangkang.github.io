dacb805e00199ba882b7d594186eca926eabf162
youdekangkang <1015493892@qq.com>
2024-06-10 19:52:06 +0800

    Site updated: 2024-06-10 19:52:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/09/机器学习训练营 幸福感比赛 学习笔记/","2024/06/09/机器学习训练营lightGBM学习笔记/","2024/06/09/机器学习训练营XGBoost学习笔记/","2024/06/09/机器学习训练营逻辑回归笔记/"];function toRandomPost(){
+var posts=["2024/06/09/机器学习训练营 幸福感比赛 学习笔记/","2024/06/09/机器学习训练营XGBoost学习笔记/","2024/06/09/机器学习训练营逻辑回归笔记/","2024/06/09/机器学习训练营lightGBM学习笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
